fe20817dde40b67efeeb100db5bb475d309f6af6
GrayB <90840369+GrayBurford@users.noreply.github.com>
2023-01-17 16:11:08 -0500

    Create 1. Find all active students.sql

---
+++ 8 kyu/1. Find all active students.sql
@@ -0,0 +1,11 @@
+Create a simple SELECT query to display student information of all ACTIVE students.
+
+TABLE STRUCTURE:
+
+students
+Id	FirstName	LastName	IsActive
+
+Note: IsActive (true or false)
+
+-- Type your query here
+SELECT * FROM students WHERE isActive;
