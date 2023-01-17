cb26ebc80af67e2ccb385d42914aaf6e337e9bc1
GrayB <90840369+GrayBurford@users.noreply.github.com>
2023-01-17 15:17:58 -0500

    Create Sum of two lowest positive integers.js

---
+++ 7 kyu/Sum of two lowest positive integers.js
@@ -0,0 +1,27 @@
+// Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. No floats or non-positive integers will be passed.
+
+// For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+
+// [10, 343445353, 3453445, 3453545353453] should return 3453455.
+
+function sumTwoSmallestNumbers(numbers) {  
+  let smallest = Infinity;
+  let secondSmallest = Infinity;
+  
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] < smallest) {
+      smallest = numbers[i];
+    }
+  }
+  
+  let idx = numbers.indexOf(smallest)
+  numbers.splice(idx,1)
+  
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] < secondSmallest) {
+      secondSmallest = numbers[i];
+    }
+  }
+  
+  return smallest + secondSmallest;
+}
